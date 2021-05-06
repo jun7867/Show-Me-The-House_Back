cf9afba6e17a3cfeb21dffc75e897c1af79266bd
jun7867 <jjune095@naver.com>
2021-05-06 13:14:36 +0900

    Initial commit

---
+++ src/main/java/com/ssafy/house/HappyHouseSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyHouseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyHouseSpringApplication.class, args);
+	}
+
+}
